12f0faf3098dfd585ce6aa3de1042ffcaa96642e
chenle02 <chenle02@gmail.com>
2025-01-12 12:52:21 -0500

    Remove tailing white spaces in normal mode by ws

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,7 +12,13 @@ keymap.set("n", "[ss", "[sz=1<CR><CR>")
 
 -- Remove trailing whitespace
 -- https://vim.fandom.com/wiki/Remove_unwanted_spaces
-keymap.set("n", "<leader>s", "mm:%s/\\s\\+$//e<CR>'m")
+keymap.set("n", "ws", function()
+  local save_pos = vim.fn.getpos(".") -- Save current cursor position
+  vim.cmd("%s/\\s\\+$//e") -- Remove trailing whitespace
+  vim.fn.setpos(".", save_pos) -- Restore cursor position
+  vim.cmd("write!")
+  vim.cmd("redraw")
+end)
 
 -- Insert blank line above or below in normal mode
 -- https://vi.stackexchange.com/questions/3875/how-to-insert-a-newline-without-leaving-normal-mode
@@ -22,3 +28,7 @@ keymap.set("n", "OO", ":<C-u>call append(line('.')-1, repeat([''], v:count1))<CR
 -- Quit Neovim
 keymap.set("n", "qq", ":q<CR>", { silent = true })
 keymap.set("n", "qw", ":wq!<CR>", { noremap = true, silent = true })
+
+-- Move selected lines upwards or downwards in visual mode
+keymap.set("x", "K", ":move '<-2<CR>gv=gv")
+keymap.set("x", "J", ":move '>+1<CR>gv=gv")
