97a73e9fa451e77c42b37b559490b23dc547f81a
chenle02 <chenle02@gmail.com>
2025-01-12 17:38:51 -0500

    Add leader o for :only and set up leader key.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,9 @@
 -- Neovim keybindings configuration
 local keymap = vim.keymap
 
+-- Set leader key
+vim.g.mapleader = "\\" -- Set leader key to backslash
+
 -- Spelling: Choose the first suggestion
 keymap.set("n", "]ss", "]sz=1<CR><CR>")
 keymap.set("n", "[ss", "[sz=1<CR><CR>")
@@ -32,3 +35,13 @@ keymap.set("n", "qw", ":wq!<CR>", { noremap = true, silent = true })
 -- Move selected lines upwards or downwards in visual mode
 keymap.set("x", "K", ":move '<-2<CR>gv=gv")
 keymap.set("x", "J", ":move '>+1<CR>gv=gv")
+
+-- Delete without yanking
+keymap.set("n", "<leader>d", '"_d')
+keymap.set("x", "<leader>d", '"_d')
+
+-- Paste without overwriting register
+keymap.set("v", "<leader>p", '"_dP')
+
+-- Close all other windows
+keymap.set("n", "<leader>o", ":only<CR>", { silent = true })
